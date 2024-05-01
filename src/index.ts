df9fb5bc512a3f19cc314694394b57ffea18672a
maitzeth <andre801mz@gmail.com>
2024-05-01 00:42:37 -0300

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { products } from './db/schema';
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+export type Env = {
+  DATABASE_URL: string;
+  NODE_ENV: string;
+}
+
+const app = new Hono<{Bindings: Env}>();
+
+app.get('/', async (c) => {
+  const sql = neon(c.env.DATABASE_URL);
+  const db = drizzle(sql);
+
+  const allProducts = await db.select().from(products);
+  return c.json(allProducts);
+});
+
+export default app;
